3b0624e3c540af01ac028893c4e139c50159ae73
Eyas Ranjous <eyas.ranjous@gmail.com>
2018-08-05 23:11:30 -0500

    add min heap key/value node

--- index.spec.js
+++ index.spec.js
@@ -12,12 +12,14 @@ describe('minHeap tests', () => {
       minHeap.insert(90);
       minHeap.insert(60);
       minHeap.insert(40);
-      minHeap.insert(20);
+      minHeap.insert(20, { name: 'test' });
     }));
 
   describe('.min()', () =>
-    it('should get the min value in the heap', () =>
-      expect(minHeap.min()).to.equal(20)));
+    it('should get the min value in the heap', () => {
+      expect(minHeap.min().getKey()).to.equal(20);
+      expect(minHeap.min().getValue()).to.deep.equal({ name: 'test' });
+    }));
 
   describe('.size()', () =>
     it('should get the size of the heap', () =>
@@ -25,31 +27,31 @@ describe('minHeap tests', () => {
 
   describe('.extractMin()', () =>
     it('should extract the min value in the heap', () => {
-      expect(minHeap.extractMin()).to.equal(20);
-      expect(minHeap.min()).to.equal(30);
+      expect(minHeap.extractMin().getKey()).to.equal(20);
+      expect(minHeap.min().getKey()).to.equal(30);
       expect(minHeap.size()).to.equal(6);
 
-      expect(minHeap.extractMin()).to.equal(30);
-      expect(minHeap.min()).to.equal(40);
+      expect(minHeap.extractMin().getKey()).to.equal(30);
+      expect(minHeap.min().getKey()).to.equal(40);
       expect(minHeap.size()).to.equal(5);
 
-      expect(minHeap.extractMin()).to.equal(40);
-      expect(minHeap.min()).to.equal(50);
+      expect(minHeap.extractMin().getKey()).to.equal(40);
+      expect(minHeap.min().getKey()).to.equal(50);
       expect(minHeap.size()).to.equal(4);
 
-      expect(minHeap.extractMin()).to.equal(50);
-      expect(minHeap.min()).to.equal(60);
+      expect(minHeap.extractMin().getKey()).to.equal(50);
+      expect(minHeap.min().getKey()).to.equal(60);
       expect(minHeap.size()).to.equal(3);
 
-      expect(minHeap.extractMin()).to.equal(60);
-      expect(minHeap.min()).to.equal(80);
+      expect(minHeap.extractMin().getKey()).to.equal(60);
+      expect(minHeap.min().getKey()).to.equal(80);
       expect(minHeap.size()).to.equal(2);
 
-      expect(minHeap.extractMin()).to.equal(80);
-      expect(minHeap.min()).to.equal(90);
+      expect(minHeap.extractMin().getKey()).to.equal(80);
+      expect(minHeap.min().getKey()).to.equal(90);
       expect(minHeap.size()).to.equal(1);
 
-      expect(minHeap.extractMin()).to.equal(90);
+      expect(minHeap.extractMin().getKey()).to.equal(90);
       expect(minHeap.min()).to.equal(null);
       expect(minHeap.size()).to.equal(0);
     }));
